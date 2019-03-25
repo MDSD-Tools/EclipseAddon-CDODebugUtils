b29aa722e139f31b212f0ff6de9a2670875d7482
Christian Petrov <cpetrov@eclipsesource.com>
2019-03-25 22:32:00 +0100

    Improve PROXY example
    Use own model.
    
    Change-Id: Ia2e16b0c1e9dd1f551e5e7d7437c2ddb96ef1a1c

--- test_projects/cdotest/src/cdotest/DemoProxyState.java
+++ test_projects/cdotest/src/cdotest/DemoProxyState.java
@@ -7,19 +7,37 @@ import org.eclipse.emf.cdo.session.CDOSession;
 import org.eclipse.emf.cdo.transaction.CDOTransaction;
 import org.eclipse.emf.cdo.util.CommitException;
 import org.eclipse.emf.cdo.util.ConcurrentAccessException;
-import org.eclipse.emf.ecore.EcoreFactory;
+
+import cDOWebPage.CDOWebPage;
+import cDOWebPage.impl.CDOWebPageFactoryImpl;
 
 public class DemoProxyState {
-	public static void main(String[] args) throws ConcurrentAccessException, CommitException, InterruptedException {
+
+	public static void main(String[] args) throws ConcurrentAccessException, CommitException {
+		CDOWebPage cdoWebPage = CDOWebPageFactoryImpl.eINSTANCE.createCDOWebPage();
+
+		String resourceId = UUID.randomUUID().toString();
+
 	    CDOSession session1 = ConnectionUtil.createConfiguration(ConnectionUtil.createConnector()).openNet4jSession();
 	    CDOSession session2 = ConnectionUtil.createConfiguration(ConnectionUtil.createConnector()).openNet4jSession();
-	    String id = UUID.randomUUID().toString();
-	    CDOTransaction transaction = session1.openTransaction();
-	    CDOResource resource = transaction.createResource(id);
-	    resource.getContents().add(EcoreFactory.eINSTANCE.createEObject());
-	    transaction.commit();
-	    CDOResource resource2 = session2.openTransaction().getResource(id);
+
+	    CDOTransaction transaction1 = session1.openTransaction();
+	    CDOResource resource1 = transaction1.createResource(resourceId);
+	    resource1.getContents().add(cdoWebPage);
+	    transaction1.commit();
+
+	    CDOTransaction trasaction2 = session2.openTransaction();
+	    CDOResource resource2 = trasaction2.getResource(resourceId);
+	    CDOWebPage cdoWebPage2 = (CDOWebPage) resource2.getContents().get(0);
+
+	    CDOTransaction transaction3 = session1.openTransaction();
+	    CDOResource resource3 = transaction3.getResource(resourceId);
+	    CDOWebPage cdoWebPage3 = (CDOWebPage) resource3.getContents().get(0);
+	    cdoWebPage3.setTitle("bar");
+	    transaction3.commit();
+
 	    System.out.println(">> SET BREAKPOINT HERE <<");
-	    // resource2 will have PROXY state (viewAndState.state)
+	    // cdoWebPage2 will have PROXY state (viewAndState.state)
 	}
+
 }
