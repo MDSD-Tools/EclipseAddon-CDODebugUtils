beffa629d623994fb63e75bb0b3186b4a2cbb940
Stephan Seifermann <stephan.seifermann@kit.edu>
2020-03-27 13:21:40 +0100

    Removed temporary files.

--- test_projects/cdoserver/de.cooperateproject.repository.commithistoryhandler/src/de/cooperateproject/repository/commithistoryhandler/protocol/CDOCommitHistoryProtocolSignal.java
+++
@@ -1,7 +0,0 @@
-package de.cooperateproject.repository.commithistoryhandler.protocol;
-
-public interface CDOCommitHistoryProtocolSignal {
-
-	public short getID();
-	
-}
--- test_projects/cdoserver/de.cooperateproject.repository.logging/src/de/cooperateproject/repository/logging/Activator.java
+++
@@ -1,7 +0,0 @@
-package de.cooperateproject.repository.logging;
-
-import de.cooperateproject.repository.logging.configurator.LogConfiguringPlugin;
-
-public class Activator extends LogConfiguringPlugin {
-
-}
